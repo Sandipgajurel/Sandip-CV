64fe72fe82ad8af7e62157de89c4cd5a008a02aa
sandip <laxmangajurel9191@gmail.com>
2022-08-07 23:12:26 +0545

    Updates

---
+++ precache-manifest.71ea3fdddda0e03ed057d9bd7c758ce2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c40bd5c6b39550a6b47a",
+    "url": "/Sandip-CV/static/css/main.c10dc6b8.chunk.css"
+  },
+  {
+    "revision": "c40bd5c6b39550a6b47a",
+    "url": "/Sandip-CV/static/js/main.c40bd5c6.chunk.js"
+  },
+  {
+    "revision": "8593b9e0c7ee8647dfca",
+    "url": "/Sandip-CV/static/js/1.8593b9e0.chunk.js"
+  },
+  {
+    "revision": "9fd8eccdfb2280eb05e0",
+    "url": "/Sandip-CV/static/js/2.9fd8eccd.chunk.js"
+  },
+  {
+    "revision": "781d9e7842d75c28fc65",
+    "url": "/Sandip-CV/static/js/runtime~main.781d9e78.js"
+  },
+  {
+    "revision": "cba0b89d2bf2d96a1ed26edb5849f804",
+    "url": "/Sandip-CV/static/media/3.cba0b89d.png"
+  },
+  {
+    "revision": "823d10a892b2f350c42c294cafb9b835",
+    "url": "/Sandip-CV/static/media/2.823d10a8.jpg"
+  },
+  {
+    "revision": "847849e19c631867f704fe2b9e70e84a",
+    "url": "/Sandip-CV/index.html"
+  }
+];
